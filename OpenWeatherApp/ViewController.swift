dc46d87c79e5b8e258e5746068d56c667ae07498
raf-wtag <fahim.rahman@welldev.io>
2021-03-29 15:24:47 +0600

    Project Initialized
    1. Will use CoreLocation for Location
    2. Will use openweathermap API for weather data

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Fahim Rahman on 29/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
