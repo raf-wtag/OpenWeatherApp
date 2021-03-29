f1da85133dc62844b87ea10d39eda98ee37b8b50
raf-wtag <fahim.rahman@welldev.io>
2021-03-29 15:48:27 +0600

    HomeView Added
    1. Delete ViewController and create HomeViewController
    2. Added label for Location Name Display
    3. Added label for Presentday's weather data display
    4. Button for weekly forecast list
    5. spinner which is visible when fetching the location and API data

---
+++ OpenWeatherApp/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Fahim Rahman on 29/3/21.
+//
+
+import UIKit
+
+
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var locationNameLabel: UILabel!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    @IBOutlet weak var presentDayForecast: UILabel!
+    @IBOutlet weak var weekForecastButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
