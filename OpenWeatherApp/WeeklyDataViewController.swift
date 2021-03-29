5f7ad18260bad89066b5e292ede800b96007779e
raf-wtag <fahim.rahman@welldev.io>
2021-03-29 16:02:06 +0600

    WeeklyView Added
    1. WeeklyViewController Added
    2. In this viewcontroller tableview containing weekly forecast will display

---
+++ OpenWeatherApp/WeeklyDataViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WeeklyDataViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Fahim Rahman on 29/3/21.
+//
+
+import UIKit
+
+class WeeklyDataViewController: UIViewController {
+    
+    
+    @IBOutlet weak var customTableLable: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
