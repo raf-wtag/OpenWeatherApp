73e335f88703b33dca7f0985b2c839940b4aca2c
raf-wtag <fahim.rahman@welldev.io>
2021-03-31 10:57:12 +0600

    API Call Changed
    Before the API network call was made from WeeklydataViewController but now it is in the HomeViewController. After checking for location permission and if user permits then fetching the location the API is being called

--- OpenWeatherApp/HomeViewController.swift
+++ OpenWeatherApp/HomeViewController.swift
@@ -24,12 +24,31 @@ class HomeViewController: UIViewController , CLLocationManagerDelegate{
     var latitude = 0.0
     var longitude = 0.0
     
+    var secretAPIKEY = ""
+    var NextSevenDaysData = [Daily]()
+    var PresentDayData = [Daily]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Load API Key from SecretKey.json File -> "APIKEY" : "secretkey"
+        if let apiData = self.readSecretKeyFile(forFileName: "Keys") {
+            if let temp = self.parseSecretKeyFile(jsonData: apiData) {
+                secretAPIKEY = temp
+            }
+        }
 
-        // Access the device location
+        // Access the device location and if successful then call API
         fetchCurrentLocation()
-       
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Again Check for location permission and similar stuffs
+        fetchCurrentLocation()
+        
     }
     
     // MARK: - Location Part
@@ -77,6 +96,20 @@ class HomeViewController: UIViewController , CLLocationManagerDelegate{
             print("Now Saving", self.longitude)
         }
         
+        // MARK: API Calling Start
+        // As now we have location now lets Call & Get Data from API Call
+        fetchAPIData(completionHandler: {
+            data in
+            self.NextSevenDaysData = data
+            
+            if !self.NextSevenDaysData.isEmpty {
+                self.PresentDayData = Array(self.NextSevenDaysData.prefix(1))
+//                print("Now printing", self.PresentDayData)
+//                print("Again Printing", self.PresentDayData[0].weather[0].description)
+                self.NextSevenDaysData.removeFirst()
+            }
+        })
+        
         // Now Stop the Spinner
         DispatchQueue.main.async {
             self.spinner.stopAnimating()
@@ -89,12 +122,84 @@ class HomeViewController: UIViewController , CLLocationManagerDelegate{
         print(error)
     }
     
+    // MARK: GET APIKEY From External File
+    // Read the .json file
+    private func readSecretKeyFile(forFileName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"), let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print("API KEY LOADING FAILED WITH ERROR", error)
+        }
+        return nil
+    }
+    
+    // Decode the Key from json
+    private func parseSecretKeyFile(jsonData: Data) -> String? {
+        do {
+            let decodedSecretKeys = try JSONDecoder().decode(SecretKeysMap.self, from: jsonData)
+            print("API key is", decodedSecretKeys.APIKEY)
+            return decodedSecretKeys.APIKEY
+        } catch {
+            print("Hey!! Error in Decoding!!")
+        }
+        return nil
+    }
+    
+    // MARK: Call & Fetch the API Data
+    func fetchAPIData(completionHandler: @escaping ([Daily]) -> Void) {
+        
+        var returnDailyData = [Daily]()
+        
+        let address = "https://api.openweathermap.org/data/2.5/onecall?lat="
+        let lat = "\(latitude)"
+        let lon = "\(longitude)"
+//        let APIKEY = "a32d1247d69743e1f60a87f3a5a904c8"
+        let APIKEY = secretAPIKEY
+        
+        print("I'm getting", latitude)
+        print("I'm getting", longitude)
+        
+        let urlString = address + lat + "&lon=" + lon + "&units=metric&exclude=current,minutely,hourly,alerts&appid=" + APIKEY
+        
+        print(urlString)
+//        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=23.8103&lon=90.4125&units=metric&exclude=current,minutely,hourly,alerts&appid=a32d1247d69743e1f60a87f3a5a904c8"
+
+        let url = URL(string: urlString)
+        
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+           
+            guard let data = data, error == nil else {
+                print("Error Occured")
+                return
+            }
+            
+            if let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) {
+                print(jsonData)
+            }
+                    
+            let result = try? JSONDecoder().decode(WeatherData.self, from: data)
+            
+            if let res = result {
+                returnDailyData = res.daily
+            }
+            
+            completionHandler(returnDailyData)
+        })
+        task.resume()
+        
+        completionHandler(returnDailyData)
+    }
+    
+    // Mark: Segue Part. From here we pass the data to the other view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let nextViewController = segue.destination as? WeeklyDataViewController
         
         if nextViewController != nil {
-            nextViewController?.latitude = self.latitude
-            nextViewController?.longitude = self.longitude
+//            nextViewController?.latitude = self.latitude
+//            nextViewController?.longitude = self.longitude
+            nextViewController?.NextSevenDaysData = self.NextSevenDaysData
         }
     }
     
--- OpenWeatherApp/WeeklyDataViewController.swift
+++ OpenWeatherApp/WeeklyDataViewController.swift
@@ -11,118 +11,21 @@ class WeeklyDataViewController: UIViewController, UITableViewDataSource, UITable
     
     @IBOutlet weak var tableView: UITableView!
     
-    var NextSevenDaysData = [Daily]() {
-        didSet {
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-    }
+//    var latitude = 0.0
+//    var longitude = 0.0
     
-    var PresentDayData = [Daily]()
-    
-    var latitude = 0.0
-    var longitude = 0.0
-    var secretAPIKEY = ""
+    var NextSevenDaysData = [Daily]() 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load API Key from SecretKey.json File -> "APIKEY" : "secretkey"
-        if let apiData = self.readSecretKeyFile(forFileName: "Keys") {
-            if let temp = self.parseSecretKeyFile(jsonData: apiData) {
-                secretAPIKEY = temp
-            }
-        }
-        
-        // Call & Get Data from API Call
-        fetchAPIData(completionHandler: {
-            data in
-            self.NextSevenDaysData = data
-            
-            if !self.NextSevenDaysData.isEmpty {
-                self.PresentDayData = Array(self.NextSevenDaysData.prefix(1))
-//                print("Now printing", self.PresentDayData)
-//                print("Again Printing", self.PresentDayData[0].weather[0].description)
-                self.NextSevenDaysData.removeFirst()
-            }
-        })
         
         // Declare the dataSource and Delegates
         tableView.dataSource = self
         tableView.delegate = self
     }
     
-    // MARK: GET APIKEY From External File
-    // Read the .json file
-    private func readSecretKeyFile(forFileName name: String) -> Data? {
-        do {
-            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"), let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
-                return jsonData
-            }
-        } catch {
-            print("API KEY LOADING FAILED WITH ERROR", error)
-        }
-        return nil
-    }
     
-    // Decode the Key from json
-    private func parseSecretKeyFile(jsonData: Data) -> String? {
-        do {
-            let decodedSecretKeys = try JSONDecoder().decode(SecretKeysMap.self, from: jsonData)
-            print("API key is", decodedSecretKeys.APIKEY)
-            return decodedSecretKeys.APIKEY
-        } catch {
-            print("Hey!! Error in Decoding!!")
-        }
-        return nil
-    }
-    
-    // MARK: Call & Fetch the API Data
-    func fetchAPIData(completionHandler: @escaping ([Daily]) -> Void) {
-        
-        var returnDailyData = [Daily]()
-        
-        let address = "https://api.openweathermap.org/data/2.5/onecall?lat="
-        let lat = "\(latitude)"
-        let lon = "\(longitude)"
-//        let APIKEY = "a32d1247d69743e1f60a87f3a5a904c8"
-        let APIKEY = secretAPIKEY
-        
-        print("I'm getting", latitude)
-        print("I'm getting", longitude)
-        
-        let urlString = address + lat + "&lon=" + lon + "&units=metric&exclude=current,minutely,hourly,alerts&appid=" + APIKEY
-        
-        print(urlString)
-//        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=23.8103&lon=90.4125&units=metric&exclude=current,minutely,hourly,alerts&appid=a32d1247d69743e1f60a87f3a5a904c8"
-
-        let url = URL(string: urlString)
-        
-        let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-           
-            guard let data = data, error == nil else {
-                print("Error Occured")
-                return
-            }
-            
-            if let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) {
-                print(jsonData)
-            }
-                    
-            let result = try? JSONDecoder().decode(WeatherData.self, from: data)
-            
-            if let res = result {
-                returnDailyData = res.daily
-            }
-            
-            completionHandler(returnDailyData)
-        })
-        task.resume()
-        
-        completionHandler(returnDailyData)
-    }
-    
+    // MARK: TableView Part
     // Core Methods of TableView
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
